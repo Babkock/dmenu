4f48e868c5f3ce57f8636d52714f4ec9bf564616
Charles Mingus <tanner.a.babcock@gmail.com>
2021-07-18 04:31:12 -0500

    Re-applied center patch

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const unsigned int alpha = 0x10;
 static const char *fonts[] = {
 	"monospace:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = "$";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
--- dmenu.c
+++ dmenu.c
@@ -27,6 +27,7 @@
 #define TEXTW(X)              (drw_fontset_getwidth(drw, (X)) + lrpad)
 
 #define OPAQUE                0xffU
+#define SCREENWIDTH           1900
 
 /* enums */
 enum { SchemeNorm, SchemeSel, SchemeOut, SchemeLast }; /* color schemes */
@@ -96,6 +97,15 @@ calcoffsets(void)
 	for (i = 0, prev = curr; prev && prev->left; prev = prev->left)
 		if ((i += (lines > 0) ? bh : MIN(TEXTW(prev->left->text), n)) > n)
 			break;
+}
+
+static int
+max_textw(void)
+{
+	int len = 0;
+	for (struct item *item = items; item && item->text; item++)
+		len = MAX(TEXTW(item->text), len);
+	return len;
 }
 
 static void
@@ -620,6 +630,9 @@ setup(void)
 	bh = drw->fonts->h + 2;
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
+
+	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
+
 #ifdef XINERAMA
 	i = 0;
 	if (parentwin == root && (info = XineramaQueryScreens(dpy, &n))) {
@@ -646,9 +659,12 @@ setup(void)
 				if (INTERSECT(x, y, 1, 1, info[i]))
 					break;
 
-		x = info[i].x_org;
-		y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
-		mw = info[i].width;
+		//x = info[i].x_org;
+		//y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
+		//mw = info[i].width;
+		mw = MIN(MAX(max_textw() + promptw, SCREENWIDTH), info[i].width);
+		x = info[i].x_org + ((info[i].width - mw) / 2);
+		y = info[i].y_org + ((info[i].height - mh) / 2);
 
 		XFree(info);
 	} else
@@ -657,11 +673,14 @@ setup(void)
 		if (!XGetWindowAttributes(dpy, parentwin, &wa))
 			die("could not get embedding window attributes: 0x%lx",
 			    parentwin);
-		x = 0;
-		y = topbar ? 0 : wa.height - mh;
-		mw = wa.width;
+		//x = 0;
+		//y = topbar ? 0 : wa.height - mh;
+		//mw = wa.width;
+		mw = MIN(MAX(max_textw() + promptw, SCREENWIDTH), wa.width);
+		x = (wa.width - mw) / 2;
+		y = (wa.height - mh) / 2;
 	}
-	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
+	//promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 	inputw = MIN(inputw, mw/3);
 	match();
 
